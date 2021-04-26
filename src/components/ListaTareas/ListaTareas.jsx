685c95fdfc0b28b84eea1354d857401866fa385d
GuillermoSM <guillermosotomarquez5@gmail.com>
2021-04-26 17:07:18 -0500

    se resuelve problema con map y return en listatareas.jsx

--- src/components/ListaTareas/ListaTareas.jsx
+++ src/components/ListaTareas/ListaTareas.jsx
@@ -1,62 +1,62 @@
 import React from 'react';
-import './ListaTareas.css'
-import Tarea from '../Tarea/Tarea'
+import './ListaTareas.css';
+import Tarea from '../Tarea/Tarea';
 
-const ListaTareas = ({tareas,setTareas,mostrarCompletadas}) => {
-      
-      const toogleCompletada = (id) =>{
-            setTareas(
-                  tareas.map((tarea)=>{
-                        if(tarea.id === id) return({...tarea, completada: !tarea.completada})
-                        return tarea;
-                  })
-            )
-      } 
+const ListaTareas = ({ tareas, setTareas, mostrarCompletadas }) => {
+	const toogleCompletada = (id) => {
+		setTareas(
+			tareas.map((tarea) => {
+				if (tarea.id === id) return { ...tarea, completada: !tarea.completada };
+				return tarea;
+			})
+		);
+	};
 
-      const actualizarTarea=(id,newTarea)=>{
-            setTareas(
-                  tareas.map((tarea)=>{
-                        if(tarea.id === id) return({...tarea, texto: newTarea})
-                        return tarea;
-                  })
-            )
-      } 
+	const actualizarTarea = (id, newTarea) => {
+		setTareas(
+			tareas.map((tarea) => {
+				if (tarea.id === id) return { ...tarea, texto: newTarea };
+				return tarea;
+			})
+		);
+	};
 
-      const eliminarTarea=(id)=>{
-         setTareas(
-               tareas.filter((tarea)=>tarea.id !== id )
-               )
-      }
+	const eliminarTarea = (id) => {
+		setTareas(tareas.filter((tarea) => tarea.id !== id));
+	};
 
-    return (
-        <ul className='lista-tareas'>
-              {
-                    tareas.length>0?
-                    tareas.map((tarea)=>{
-                                  if(mostrarCompletadas)
-                                  {                    
-                                        return <Tarea key={tarea.id} 
-                                               tarea={tarea} 
-                                               toogleCompletada={toogleCompletada}
-                                               actualizarTarea={actualizarTarea}
-                                               eliminarTarea={eliminarTarea}
-                                               />
-                                  }
-                                    else if(!tarea.completada){
-                                          return <Tarea key={tarea.id} 
-                                                 tarea={tarea} 
-                                                 toogleCompletada={toogleCompletada}
-                                                 actualizarTarea={actualizarTarea}
-                                                 eliminarTarea={eliminarTarea}
-                                                 / >
-                                    } 
-                                    
-                              })
-                    :
-                    <div className='lista-tareas__mensaje'>Agrega una tarea</div>
-              }
-        </ul>
-      );
-}
- 
+	return (
+		<ul className='lista-tareas'>
+			{tareas.length > 0 ? (
+				tareas.map((tarea) => {
+					if (mostrarCompletadas) {
+						return (
+							<Tarea
+								key={tarea.id}
+								tarea={tarea}
+								toogleCompletada={toogleCompletada}
+								actualizarTarea={actualizarTarea}
+								eliminarTarea={eliminarTarea}
+							/>
+						);
+					} else if (!tarea.completada) {
+						return (
+							<Tarea
+								key={tarea.id}
+								tarea={tarea}
+								toogleCompletada={toogleCompletada}
+								actualizarTarea={actualizarTarea}
+								eliminarTarea={eliminarTarea}
+							/>
+						);
+					}
+					return '';
+				})
+			) : (
+				<div className='lista-tareas__mensaje'>Agrega una tarea</div>
+			)}
+		</ul>
+	);
+};
+
 export default ListaTareas;
